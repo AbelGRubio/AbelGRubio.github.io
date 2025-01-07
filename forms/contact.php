bb75be901c739b7349d3b5e8033dd7c97fb26c84
abel.garcia <ablg11673@gmail.com>
2025-01-07 11:15:21 +0100

    fix(abel): update website and form contact

--- forms/contact.php
+++ forms/contact.php
@@ -17,7 +17,7 @@
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
+
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
---
+++ forms/form-handler.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("contactForm");
+
+  form.addEventListener("submit", async function (event) {
+    event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+
+    const email = document.getElementById("email").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    const payload = {
+      email: email,
+      subject: subject,
+      message: message,
+    };
+
+    // Mostrar el mensaje de "Loading"
+    document.querySelector(".loading").style.display = "block";
+    document.querySelector(".error-message").style.display = "none";
+    document.querySelector(".sent-message").style.display = "none";
+
+    try {
+      // Realiza la solicitud POST a la API
+
+      const response = await fetch("http://localhost:10000/send-email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      });
+
+      document.querySelector(".loading").style.display = "none"; // Ocultar "Loading"
+
+      if (response.ok) {
+        document.querySelector(".sent-message").style.display = "block";
+      } else {
+        const errorData = await response.json();
+        document.querySelector(".error-message").innerHTML =
+          `Error: ${errorData.detail || "Error desconocido"}`;
+        document.querySelector(".error-message").style.display = "block";
+      }
+    } catch (error) {
+      document.querySelector(".loading").style.display = "none"; // Ocultar "Loading"
+      document.querySelector(".error-message").innerHTML =
+        `Error: ${error.message}`;
+      document.querySelector(".error-message").style.display = "block";
+    }
+  });
+});
